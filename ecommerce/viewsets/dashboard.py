6a31cdd41b121e79c1160b4bb16d90610cb19b11
tavoxr <tavoxr23@gmail.com>
2021-08-08 05:19:00 -0600

    admin authentication

--- ecommerce/viewsets/dashboard.py
+++ ecommerce/viewsets/dashboard.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from ..models import *
+from ..decorators import admin_only
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='login')
+@admin_only
 def dashboard(request):
 
     context = {}
