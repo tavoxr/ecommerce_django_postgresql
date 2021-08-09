ddf8f6b2b2ef8ef9a289fb10fe9cd23dcc865b16
tavoxr <tavoxr23@gmail.com>
2021-08-09 02:46:39 -0600

    paypal payment

--- ecommerce/viewsets/dashboard.py
+++ ecommerce/viewsets/dashboard.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from ..models import *
+from django.contrib.auth.models import Group, User
 from ..decorators import admin_only
 from django.contrib.auth.decorators import login_required
 
@@ -7,8 +8,14 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 @admin_only
 def dashboard(request):
-    
+    group = Group.objects.get(name = 'customer')
+    user = request.user
+    customers = User.objects.filter(groups = group )
+    orders =  Order.objects.filter(user = user )
     
 
-    context = {}
+    context = {
+        'customers': customers,
+        'orders': orders,
+    }
     return render(request, 'ecommerce/pages/Dashboard.html', context)
